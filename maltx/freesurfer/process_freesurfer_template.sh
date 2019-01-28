1b3eafdbaf70bab9a042b4313530b5f20f8873cb
RobertJirsaraie <robertjirsaraie21@gmail.com>
2019-01-28 05:55:51 +0000

    Submits Jobs to Process raw T1-weighted Images

---
+++ maltx/freesurfer/process_freesurfer_template.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#$ -N fs6_SUBID
+#$ -q som,asom*,free*,pub*
+#$ -ckpt blcr
+#$ -l kernel=blcr
+#$ -r y
+#$ -m e
+#$ -cwd
+#$ -notify
+###########################################
+### Load Software and Define Root Paths ###
+###########################################
+
+module purge
+module load freesurfer/6.0
+export SUBJECTS_DIR=/dfs1/som/rao_col/maltx/analysis/freesurfer
+
+############################################################
+### Create MGZ Files If Needed For Freesurfer Processing ###
+############################################################
+
+if [ -a /dfs1/som/rao_col/maltx/bids/sub-SUBID/ses-1/anat/sub-SUBID_ses-1_T1w.nii.gz ]; then 
+
+	mri_convert /dfs1/som/rao_col/maltx/bids/sub-SUBID/ses-1/anat/sub-SUBID_ses-1_T1w.nii.gz /dfs1/som/rao_col/maltx/analysis/mgz/sub-SUBID_ses-1_T1w_converted.mgz
+else
+		
+	mri_convert /dfs1/som/rao_col/maltx/bids/sub-SUBID/ses-1/anat/sub-SUBID_ses-1_run-02_T1w.nii.gz /dfs1/som/rao_col/maltx/analysis/mgz/sub-SUBID_ses-1_T1w_converted.mgz
+fi
+
+#################################################################
+### Define Paths to Subjects' Scans That Need to be Processed ###
+#################################################################
+
+recon-all -i /dfs1/som/rao_col/maltx/analysis/mgz/sub-SUBID_ses-1_T1w_converted.mgz -s SUBID -all -qcache -3T -hippocampal-subfields-T1 -brainstem-structures
+
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡ #####
+###################################################################################################
